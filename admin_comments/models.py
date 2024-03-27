ec319dcd6e3f23838977631742053e871a77366f
arjoonn sharma <arjoonn@midpathsoftware.com>
2024-03-27 12:54:06 +0530

    x

--- admin_comments/models.py
+++ admin_comments/models.py
@@ -8,7 +8,8 @@ from django.contrib.contenttypes.fields import GenericForeignKey
 class Comment(models.Model):
     # Support integer and UUID primary keys
     object_id = models.CharField(
-        max_length=36  # 36 chosen to match UUID length (RFC4122)
+        max_length=36,  # 36 chosen to match UUID length (RFC4122)
+        primary_key=True,
     )
     content_object = GenericForeignKey("content_type", "object_id")
     content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
@@ -25,7 +26,8 @@ class Comment(models.Model):
 class Attachment(models.Model):
     # Support integer and UUID primary keys
     object_id = models.CharField(
-        max_length=36  # 36 chosen to match UUID length (RFC4122)
+        max_length=36,  # 36 chosen to match UUID length (RFC4122)
+        primary_key=True,
     )
     content_object = GenericForeignKey("content_type", "object_id")
     content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
