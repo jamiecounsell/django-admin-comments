01f9807c3973856b094356b8b433d70ead17b5a8
arjoonn sharma <arjoonn@midpathsoftware.com>
2024-03-27 12:32:48 +0530

    x

--- admin_comments/admin.py
+++ admin_comments/admin.py
@@ -1,49 +1,53 @@
 from django.contrib.contenttypes.admin import GenericTabularInline
-from admin_comments.models import Comment
+from admin_comments.models import Comment, Attachment
 from admin_comments.helpers import get_class_from_str
 from django.conf import settings
 
 
-class CommentInline(GenericTabularInline):
-
-    model = Comment
-
-    readonly_fields = ('user',)
-
-    classes = ['collapse']
+class BaseInline:
+    readonly_fields = ("user",)
+    classes = ["collapse"]
 
     def __init__(self, *args, **kwargs):
         form_klass = getattr(
             settings,
-            'ADMIN_COMMENTS_FORM_CLASS',
-            'admin_comments.forms.CommentInlineForm')
+            f"ADMIN_{self.kind.upper()}_FORM_CLASS",
+            f"admin_comments.forms.{self.kind.capitalize()}InlineForm",
+        )
 
         formset_klass = getattr(
             settings,
-            'ADMIN_COMMENTS_FORMSET_CLASS',
-            'admin_comments.forms.CommentInlineFormset')
+            f"ADMIN_{self.kind.upper()}_FORMSET_CLASS",
+            f"admin_comments.forms.{self.kind.capitalize()}InlineFormset",
+        )
 
-        SHOW_EMPTY = getattr(settings, 'ADMIN_COMMENTS_SHOW_EMPTY', False)
+        SHOW_EMPTY = getattr(settings, "ADMIN_COMMENTS_SHOW_EMPTY", False)
 
-        CommentForm = get_class_from_str(form_klass)
-        CommentFormSet = get_class_from_str(formset_klass)
-
-        self.form = CommentForm
-        self.formset = CommentFormSet
+        self.form = get_class_from_str(form_klass)
+        self.formset = get_class_from_str(formset_klass)
         self.extra = 1 if SHOW_EMPTY else 0
-        super(CommentInline, self).__init__(*args, **kwargs)
+        super().__init__(*args, **kwargs)
 
     def get_queryset(self, request):
-        return super(CommentInline, self).get_queryset(request).order_by('-time')
+        return super().get_queryset(request).order_by("-time")
 
     def has_delete_permission(self, request, obj=None):
         return False
 
     def get_formset(self, request, obj=None, **kwargs):
-        formset = super(CommentInline, self).get_formset(
-            request, obj, **kwargs)
+        formset = super().get_formset(request, obj, **kwargs)
         formset.current_user = request.user
         return formset
 
 
-__all__ = ['CommentInline']
+class CommentInline(BaseInline, GenericTabularInline):
+    kind = "Comment"
+    model = Comment
+
+
+class AttachmentInline(BaseInline, GenericTabularInline):
+    kind = "Attachment"
+    model = Attachment
+
+
+__all__ = ["CommentInline", "AttachmentInline"]
--- admin_comments/forms.py
+++ admin_comments/forms.py
@@ -1,32 +1,57 @@
 from django import forms
 from django.contrib.contenttypes.admin import BaseGenericInlineFormSet
-from admin_comments.models import Comment
+from admin_comments.models import Comment, Attachment
 
 
 class CommentInlineForm(forms.ModelForm):
     def __init__(self, *args, **kwargs):
         super(CommentInlineForm, self).__init__(*args, **kwargs)
-        instance = getattr(self, 'instance', None)
-        self.fields['comment'].widget.attrs['rows'] = 4
+        instance = getattr(self, "instance", None)
+        self.fields["comment"].widget.attrs["rows"] = 4
         if instance and instance.pk:
-            self.fields['comment'].widget.attrs['readonly'] = True
-            self.fields['comment'].widget.attrs['border'] = 0
+            self.fields["comment"].widget.attrs["readonly"] = True
+            self.fields["comment"].widget.attrs["border"] = 0
 
     class Meta:
         model = Comment
         exclude = []
 
     class Media:
-        css = {
-            'all': ('css/admin_comments.css',)
-        }
+        css = {"all": ("css/admin_comments.css",)}
 
 
 class CommentInlineFormset(BaseGenericInlineFormSet):
     def save_new(self, form, commit=True):
         setattr(form.instance, "user", self.current_user)
-        return super(CommentInlineFormset, self).save_new(
-            form, commit=True)
+        return super(CommentInlineFormset, self).save_new(form, commit=True)
 
 
-__all__ = ['CommentInlineForm', 'CommentInlineFormset']
+class AttachmentInlineForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super(AttachmentInlineForm, self).__init__(*args, **kwargs)
+        instance = getattr(self, "instance", None)
+        self.fields["comment"].widget.attrs["rows"] = 4
+        if instance and instance.pk:
+            self.fields["comment"].widget.attrs["readonly"] = True
+            self.fields["comment"].widget.attrs["border"] = 0
+
+    class Meta:
+        model = Attachment
+        exclude = []
+
+    class Media:
+        css = {"all": ("css/admin_comments.css",)}
+
+
+class AttachmentInlineFormset(BaseGenericInlineFormSet):
+    def save_new(self, form, commit=True):
+        setattr(form.instance, "user", self.current_user)
+        return super(AttachmentInlineFormset, self).save_new(form, commit=True)
+
+
+__all__ = [
+    "CommentInlineForm",
+    "CommentInlineFormset",
+    "AttachmentInlineForm",
+    "AttachmentInlineFormset",
+]
